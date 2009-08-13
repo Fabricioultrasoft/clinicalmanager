766da5e53cc30b0a8fc804c3e7dc066899b03fb7
renalexster <renalexster@bdac4950-76eb-11de-94da-c3e9e1814b9e>
2009-08-13 15:06:43 +0000

    Criação de classes AcessoDados e Negocio.

--- Fonte/Clinicalmanager/AcessoDados/RepositorioMedico.cs
+++ Fonte/Clinicalmanager/AcessoDados/RepositorioMedico.cs
@@ -2,10 +2,95 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Classes_Básicas;
+using Npgsql;
+using System.Data;
 namespace AcessoDados
 {
-    class RepositorioMedico
+    class RepositorioMedico:Conexao
     {
+        NpgsqlCommand cmd;
+        NpgsqlDataReader reader;
+
+        public void inserir(Medico medico)
+        {
+            if (medico != null)
+            {
+                try
+                {
+                    string cmdStr = "insert into clinicalmanager.medico(nome) values (@nome)";
+                    base.conn.Open();
+                    cmd = base.conn.CreateCommand();
+                    cmd.CommandText = cmdStr;
+                    cmd.Parameters.Add("@nome", medico.Nome);
+                    cmd.ExecuteNonQuery();
+                    base.conn.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception("Erro ao incluir paciente");
+                }
+            }
+        }
+
+        public void atualizar(Medico medico)
+        {
+            string cmdStr = "update clinicalmanager.paciente set nome=@nome where idpad = @idpac";
+            try
+            {
+                base.conn.Open();
+                cmd = base.conn.CreateCommand();
+                cmd.CommandText = cmdStr;
+                //cmd.Parameters.Add("@idpac", medico);
+                cmd.Parameters.Add("@nome", medico.Nome);
+                cmd.ExecuteNonQuery();
+                base.conn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Não foi possível remover o paciente");
+            }
+        }
+
+        public void exluir(Medico medico)
+        {
+            string cmdStr = "delete from clinicalmanager.paciente where idpad = @idpac";
+            try
+            {
+                base.conn.Open();
+                cmd = base.conn.CreateCommand();
+                cmd.CommandText = cmdStr;
+                //cmd.Parameters.Add("@idpac", medico.Idpac);
+                cmd.ExecuteNonQuery();
+                base.conn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Não foi possível remover o paciente");
+            }
+        }
+
+        public Paciente consultar(string nome)
+        {
+            string sql = "select idpac,nome from clinicalmanager where nome like '?nome'";
+            cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.Parameters.Add("?nome", nome);
+            reader = base.execute(cmd);
+            Paciente output = new Paciente();
+            output.Nome = reader.GetString(1);
+            output.Idpac = reader.GetInt32(0);
+            return output;
+            throw new NotImplementedException();
+        }
+
+        public DataSet consultarTodos()
+        {
+            string sql = "select * from clinicalmanager.paciente";
+            Npgsql.NpgsqlCommand cmd = base.conn.CreateCommand();
+            //cmd.CommandText = 
+            return base.execute(sql);
+        }
     }
 }
